5f9eca11ed4f0ed8d4220b31aa5ad39af0caec3c
Ben Ferrer <emagus@gmail.com>
2014-06-04 17:04:17 -0400

    work on fragment layouts, and initial coding

--- NotesTest/src/main/java/com/randomstuff/notestest/NoteFragment.java
+++ NotesTest/src/main/java/com/randomstuff/notestest/NoteFragment.java
@@ -1,6 +1,18 @@
 package com.randomstuff.notestest;
 
 import android.app.Fragment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 
 public class NoteFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+
+        View result = inflater.inflate(R.layout.notes_detail, container, false);
+        return(result);
+    }
 }
--- NotesTest/src/main/java/com/randomstuff/notestest/NoteListFragment.java
+++ NotesTest/src/main/java/com/randomstuff/notestest/NoteListFragment.java
@@ -1,6 +1,19 @@
 package com.randomstuff.notestest;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.View;
 
 public class NoteListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View result = inflater.inflate(R.layout.notes_list, container, false);
+
+        return(result);
+
+    }
 }
