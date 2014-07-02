f6a364e73600f6006f7cba7df5a7d53876507bbc
Ben Ferrer <emagus@gmail.com>
2014-07-02 16:16:23 -0400

    possible fix for new notes, and beginning work on database with date modified

--- NotesTest/src/main/java/com/randomstuff/notestest/DatabaseHelper.java
+++ NotesTest/src/main/java/com/randomstuff/notestest/DatabaseHelper.java
@@ -6,13 +6,14 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "notestest.db";
-    private static final int SCHEMA_VERSION = 1;
+    private static final int SCHEMA_VERSION = 2;
     private Context ctxt=null;
 
     private static final String TABLE_NAME ="notes";
     private static final String COLUMN_ID = "_id";
     private static final String COLUMN_TITLE = "title";
     private static final String COLUMN_NOTE = "note";
+    private static final String COLUMN_NOTE_MODIFIED = "note_mod";
 
     public DatabaseHelper(Context ctxt) {
         super(ctxt, DATABASE_NAME, null, SCHEMA_VERSION);
@@ -27,7 +28,8 @@ class DatabaseHelper extends SQLiteOpenHelper {
             db.execSQL("CREATE TABLE " + TABLE_NAME + " (" +
                     COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     COLUMN_TITLE + " TEXT, " +
-                    COLUMN_NOTE + " TEXT);");
+                    COLUMN_NOTE + " TEXT, " +
+                    COLUMN_NOTE_MODIFIED + "TEXT);");
             db.setTransactionSuccessful();
         } finally {
             db.endTransaction();
@@ -38,6 +40,12 @@ class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         //tell it that if its upgrading, monkeys in the system
-        throw new RuntimeException(ctxt.getString(R.string.sql_upgrade_error));
+        //throw new RuntimeException(ctxt.getString(R.string.sql_upgrade_error));
+        switch (oldVersion) {
+            case (1):
+                db.execSQL("ALTER TABLE " +
+                        TABLE_NAME + " ADD COLUMN " +
+                        COLUMN_NOTE_MODIFIED + " TEXT");
+        }
     }
 }
